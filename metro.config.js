85d57908deb4b706acf0754a575cbdf22f9a5b70
Shane Wilkerson <shanewilkerson4@gmail.com>
2025-07-25 18:55:21 -0700

    Working build tested on TestFlight

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = mergeConfig(defaultConfig, {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-}); 
